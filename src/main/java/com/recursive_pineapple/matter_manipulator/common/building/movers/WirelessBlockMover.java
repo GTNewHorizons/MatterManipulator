e4a969b694a380e3514d1238d6f0fc678320719b
Kogepan229 <73163994+Kogepan229@users.noreply.github.com>
2026-05-20 21:29:16 +0900

    Port AE2Stuff Wireless Connector/Hub to AE2 (#50)

--- src/main/java/com/recursive_pineapple/matter_manipulator/common/building/movers/WirelessBlockMover.java
+++ src/main/java/com/recursive_pineapple/matter_manipulator/common/building/movers/WirelessBlockMover.java
@@ -1,6 +1,6 @@
 package com.recursive_pineapple.matter_manipulator.common.building.movers;
 
-import net.minecraft.tileentity.TileEntity;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
 import net.minecraftforge.oredict.OreDictionary;
@@ -8,56 +8,50 @@ import net.minecraftforge.oredict.OreDictionary;
 import com.recursive_pineapple.matter_manipulator.common.building.InteropConstants;
 import com.recursive_pineapple.matter_manipulator.common.building.PendingMove;
 
-import net.bdew.ae2stuff.machines.wireless.TileWireless;
-import net.bdew.lib.block.BlockRef;
-
 public class WirelessBlockMover extends StandardBlockMover {
 
     public static final WirelessBlockMover INSTANCE = new WirelessBlockMover();
 
     @Override
     public boolean canMove(World world, int x, int y, int z) {
-        return InteropConstants.WIRELESS_CONNECTOR.matches(world.getBlock(x, y, z), OreDictionary.WILDCARD_VALUE);
+        return InteropConstants.isWirelessConnector(world.getBlock(x, y, z), OreDictionary.WILDCARD_VALUE);
     }
 
     @Override
     public void place(PendingMove pendingMove, World world, int x, int y, int z, StandardBlock standardBlock) {
+        if (pendingMove != null) {
+            adjustWirelessLinkNbt(standardBlock, pendingMove);
+        }
+
         super.place(pendingMove, world, x, y, z, standardBlock);
-
-        if (pendingMove == null) return;
-
-        TileEntity te = world.getTileEntity(x, y, z);
-        if (te == null) return;
-
-        adjustWirelessLink((TileWireless) te, pendingMove);
     }
 
-    private void adjustWirelessLink(TileWireless wireless, PendingMove pendingMove) {
-        scala.Option<BlockRef> linkOpt = wireless.link().value();
+    private void adjustWirelessLinkNbt(StandardBlock standardBlock, PendingMove pendingMove) {
+        NBTTagCompound tileData = standardBlock.tileData();
+        if (tileData == null || !tileData.hasKey("connectedTargets")) return;
 
-        if (linkOpt.isEmpty()) return;
+        NBTTagCompound connectedTargets = tileData.getCompoundTag("connectedTargets");
 
-        BlockRef ref = linkOpt.get();
-        int lx = ref.x();
-        int ly = ref.y();
-        int lz = ref.z();
+        int i = 0;
+        while (connectedTargets.hasKey("pos#" + i)) {
+            NBTTagCompound coord = connectedTargets.getCompoundTag("pos#" + i);
+            int lx = coord.getInteger("x");
+            int ly = coord.getInteger("y");
+            int lz = coord.getInteger("z");
 
-        // only adjust if the linked partner was within the source region (i.e. it's also being moved)
-        if (
-            lx >= pendingMove.getSrcMinX() && lx <= pendingMove.getSrcMaxX() &&
-                ly >= pendingMove.getSrcMinY() &&
-                ly <= pendingMove.getSrcMaxY() &&
-                lz >= pendingMove.getSrcMinZ() &&
-                lz <= pendingMove.getSrcMaxZ()
-        ) {
-            wireless.link()
-                .set(
-                    new BlockRef(
-                        lx + pendingMove.getMoveOffsetX(),
-                        ly + pendingMove.getMoveOffsetY(),
-                        lz + pendingMove.getMoveOffsetZ()
-                    )
-                );
+            if (
+                lx >= pendingMove.getSrcMinX() && lx <= pendingMove.getSrcMaxX() &&
+                    ly >= pendingMove.getSrcMinY() &&
+                    ly <= pendingMove.getSrcMaxY() &&
+                    lz >= pendingMove.getSrcMinZ() &&
+                    lz <= pendingMove.getSrcMaxZ()
+            ) {
+                coord.setInteger("x", lx + pendingMove.getMoveOffsetX());
+                coord.setInteger("y", ly + pendingMove.getMoveOffsetY());
+                coord.setInteger("z", lz + pendingMove.getMoveOffsetZ());
+            }
+
+            i++;
         }
     }
 }
--- src/main/java/com/recursive_pineapple/matter_manipulator/common/utils/Mods.java
+++ src/main/java/com/recursive_pineapple/matter_manipulator/common/utils/Mods.java
@@ -15,7 +15,6 @@ import org.jetbrains.annotations.NotNull;
 public enum Mods implements IMod, ITargetMod {
 
     AE2FluidCraft(Names.AE2_FLUID_CRAFT),
-    AE2Stuff(Names.AE2STUFF),
     Angelica(Names.ANGELICA),
     AppliedEnergistics2(Names.APPLIED_ENERGISTICS2),
     ArchitectureCraft(Names.ARCHITECTURE_CRAFT),
@@ -64,7 +63,6 @@ public enum Mods implements IMod, ITargetMod {
     public static class Names {
 
         public static final String AE2_FLUID_CRAFT = "ae2fc";
-        public static final String AE2STUFF = "ae2stuff";
         public static final String ANGELICA = "angelica";
         public static final String APPLIED_ENERGISTICS2 = "appliedenergistics2";
         public static final String ARCHITECTURE_CRAFT = "ArchitectureCraft";
